85b25042a41a2aeeb39105203870321607e6f25b
Asmee Mishra <asmeemishra@asmees-air.home>
2024-09-20 12:45:04 +0100

    better ticker

--- app/[ticker].tsx
+++ app/[ticker].tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, View } from 'react-native';
-import { useLocalSearchParams } from 'expo-router';
+import { StyleSheet, View, Pressable} from 'react-native';
+import { useLocalSearchParams, router } from 'expo-router';
 import { Text } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
@@ -8,19 +8,20 @@ export default function TickerScreen() {
 
 
   return (
-    <Pressable onPress={() => router.back()}>
+    <View
+        style={{
+          flexDirection: "row",
+          paddingVertical: 25,
+          justifyContent: "space-between",
+        }}
+      >
+        <Pressable onPress={() => router.back()}>
           <MaterialCommunityIcons
             name="chevron-left"
             color={"white"}
             size={40}
           />
-      </Pressable>
-        
-          <MaterialCommunityIcons
-            name={
-              likedStocks.includes(ticker as string) ? "star" : "star-outline"
-            }
-            color={"white"}
-            size={40}
-          />
-          )
+        </Pressable>
+    </View>
+  )
+}
